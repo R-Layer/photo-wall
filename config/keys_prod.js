49712e350cf076e705e4782f11e90125f4a71d88
R-Layer <layer@europe.com>
2018-08-10 11:42:59 +0200

    deploy stuff

--- config/keys_prod.js
+++ config/keys_prod.js
@@ -1,5 +1,7 @@
 module.exports = {
   PORT: process.env.PORT,
   MONGODB_URI: process.env.MONGODB_URI,
-  JWT_SECRET: process.env.JWT_SECRET
+  JWT_SECRET: process.env.JWT_SECRET,
+  GITHUB_CLIENT_ID: process.env.GITHUB_CLIENT_ID,
+  GITHUB_CLIENT_SECRET: process.env.GITHUB_CLIENT_SECRET
 };
